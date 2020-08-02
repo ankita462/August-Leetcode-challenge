3d959a2c56fe8bfa331b3ede356f37c1961ab225
Ankita Garg <44531331+ankita462@users.noreply.github.com>
2020-08-02 13:09:32 +0530

    Create 2_Design HashSet.cpp
    Design a HashSet without using any built-in hash table libraries.
    
    To be specific, your design should include these functions:
    
    add(value): Insert a value into the HashSet. 
    contains(value) : Return whether the value exists in the HashSet or not.
    remove(value): Remove a value in the HashSet. If the value does not exist in the HashSet, do nothing.
    
    Example:
    
    MyHashSet hashSet = new MyHashSet();
    hashSet.add(1);         
    hashSet.add(2);         
    hashSet.contains(1);    // returns true
    hashSet.contains(3);    // returns false (not found)
    hashSet.add(2);          
    hashSet.contains(2);    // returns true
    hashSet.remove(2);          
    hashSet.contains(2);    // returns false (already removed)
    
    Note:
    
    All values will be in the range of [0, 1000000].
    The number of operations will be in the range of [1, 10000].
    Please do not use the built-in HashSet library.

---
+++ 2_Design HashSet.cpp
@@ -0,0 +1,29 @@
+class MyHashSet {
+public:
+    /** Initialize your data structure here. */
+    vector<int> v;
+    MyHashSet() {
+        v.resize(100000,0);
+    }
+    
+    void add(int key) {
+        v[key]=1;
+    }
+    
+    void remove(int key) {
+        v[key]=0;
+    }
+    
+    /** Returns true if this set contains the specified element */
+    bool contains(int key) {
+        return v[key];
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
